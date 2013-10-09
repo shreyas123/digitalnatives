09deaecfa63d42adb7d4498bfeddb290587ea94b
shreyasagarwal <shreyas.agarwal@artirix.com>
2013-10-09 20:21:46 +0530

    Added scope by_start_name

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,6 @@ class Role < ActiveRecord::Base
   has_many :users
   has_many :permission_roles
   has_many :permissions, through: :permission_roles
+
+  scope :by_start_name, lambda {  |*char| {:conditions => "name like '#{char.first}%'"} }
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -6,4 +6,17 @@ describe Role do
 		it { should have_many :permissions }
 		it { should have_many :users }
 	end
+
+	describe '#by_start_name' do 
+		before do 
+			FactoryGirl.create :role, name: 'Shreyas'
+			FactoryGirl.create :role, name: 'Test'
+			FactoryGirl.create :role, name: 'Test1'
+			FactoryGirl.create :role, name: 'Digital'
+		end
+		it 'returns roles with a beginning character in params' do 
+			expect(Role.all.length).to eq 4
+			expect(Role.by_start_name('S').length).to eq 1
+		end
+	end
 end
