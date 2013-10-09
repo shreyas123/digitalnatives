8da81a188ffe3ed3f250e592a579fec5d5357e15
shreyasagarwal <shreyas.agarwal@artirix.com>
2013-10-09 19:30:53 +0530

    Added role model and association

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,7 @@
 class Role < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :users
+  has_many :permission_roles
+  has_many :permissions, through: :permission_roles
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Role do 
 	describe '#associations' do 
+		it { should have_many :permission_roles }
 		it { should have_many :permissions }
+		it { should have_many :users }
 	end
 end
